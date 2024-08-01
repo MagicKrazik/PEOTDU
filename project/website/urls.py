15764e0c4ddf857be94a6259827a3d27fb69dfeb
Carlos <carlos.horacio.olivares@gmail.com>
2024-08-01 12:52:29 -0600

    First draft finished map project

--- project/website/urls.py
+++ project/website/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('mapa/', views.mapa, name='mapa'),
     path('audiencias/', views.audiencias, name='audiencias'),
-]    
+]
--- project/website/views.py
+++ project/website/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+import os
+from django.conf import settings
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
-
-def mapa(request):
-    return render(request, 'mapa.html')
 
 def audiencias(request):
     return render(request, 'audiencias.html')
 
+def get_kml_files():
+    kml_dir = os.path.join(settings.STATICFILES_DIRS[0], 'kml')
+    kml_files = [f for f in os.listdir(kml_dir) if f.endswith('.kml')]
+    
+    # Add labels for KML files
+    kml_data = [
+        {
+            'filename': f,
+            'label': f.replace('.kml', '').replace('_', ' ').title(),
+            'preselected': f == 'Mapa_Base.kml'
+        } for f in kml_files
+    ]
+    return kml_data
+
+def mapa(request):
+    kml_files = get_kml_files()
+    return render(request, 'mapa.html', {'kml_files': kml_files})
